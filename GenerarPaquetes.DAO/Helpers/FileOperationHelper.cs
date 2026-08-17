c9407fc303c73431abfe4172a8d495f8e16429cd
DAY-V <DAY-V@DESKTOP-PLM74CT>
2026-08-17 12:06:31 -0600

    Se crea la estructura del Helper con un metodo para copiar la estructura del directorio

--- GenerarPaquetes.DAO/Helpers/FileOperationHelper.cs
+++ GenerarPaquetes.DAO/Helpers/FileOperationHelper.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace GenerarPaquetes.DAO.Helpers
 {
-    internal class FileOperationHelper
+    public static class FileOperationHelper
     {
+        public static void CopiarEstructuraDelDirectorio(string origen, string destino) //Metodo que copia toda la estructura que tiene el directorio que se necesita
+        {
+            Directory.CreateDirectory(destino);
+
+            foreach (string archivo in Directory.GetFiles(origen))
+            {
+                string archivoObjetivo = Path.Combine(destino, Path.GetFileName(archivo));
+                File.Copy(archivo, archivoObjetivo, true);
+            }
+            
+            foreach(string subdirectorio in Directory.GetDirectories(origen))
+            {
+                string subdireObjetivo = Path.Combine(destino, Path.GetFileName(subdirectorio));
+                CopiarEstructuraDelDirectorio(subdirectorio, subdireObjetivo);
+            }    
+        }
     }
 }
