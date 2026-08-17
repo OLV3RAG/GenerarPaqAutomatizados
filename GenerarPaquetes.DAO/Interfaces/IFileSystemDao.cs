2ba799f0ee15f9ef995d65ca2d0292064275a399
DAY-V <DAY-V@DESKTOP-PLM74CT>
2026-08-17 11:48:05 -0600

    Comentarios sobre cada metodo en las interfaces

--- GenerarPaquetes.DAO/Interfaces/IFileSystemDao.cs
+++ GenerarPaquetes.DAO/Interfaces/IFileSystemDao.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace GenerarPaquetes.DAO.Interfaces
 {
-    internal interface IFileSystemDao
+    public interface IFileSystemDao
     {
-        void CrearDirectorio(string ruta);
-        bool ExisteArchivo(string ruta);
-        bool ExisteDirectorio(string ruta);
-        void CopiarArchivo(string origen, string destino, bool sobreescribir = true);
-        void CopiarDirectorioRecursivo(string origen, string destino);
-        string[] LeerLineasDeArchivo(string ruta);
-        void EscribirLineasDeArchivo(string ruta, string[] lineas);
-        string[] ObtenerArchivosDelDirectorio(string ruta, string patronBusqueda);
-        string[] ObtenerLosSubdirectorios(string ruta, string patronBusqueda, SearchOption op = SearchOption.TopDirectoryOnly);
+        void CrearDirectorio(string ruta); //Crea los directorios existentes
+        bool ExisteArchivo(string ruta); //Verficia si los archivos existen
+        bool ExisteDirectorio(string ruta); //Verifica si el directorio existe
+        void CopiarArchivo(string origen, string destino, bool sobreescribir = true); //Copia los archivos correspondientes
+        void CopiarDirectorioRecursivo(string origen, string destino); //Copiar el directorio que se necesita
+        string[] LeerLineasDeArchivo(string ruta); //Leer el contenido de cada archivo
+        void EscribirLineasDeArchivo(string ruta, string[] lineas); //Escribe las lineas del archivo
+        string[] ObtenerArchivosDelDirectorio(string ruta, string patronBusqueda); //Obtiene los archivos necesarios del directorio
+        string[] ObtenerLosSubdirectorios(string ruta, string patronBusqueda, SearchOption op = SearchOption.TopDirectoryOnly); //Obtiene los subdirectorios necesarios
     }
 }
--- GenerarPaquetes.DAO/Interfaces/ITfsMockDao.cs
+++ GenerarPaquetes.DAO/Interfaces/ITfsMockDao.cs
@@ -8,8 +8,8 @@ namespace GenerarPaquetes.DAO.Interfaces
 {
     internal interface ITfsMockDao
     {
-        bool ExisteBuild(string rutaTfsBase, int numeroBuild);
-        string ObtenerRutaBuild(string rutaTfsBase, int numeroBuild);
-        List<string> BuscarCompilados(string rutaBuild, string nombreModulo);
+        bool ExisteBuild(string rutaTfsBase, int numeroBuild); //Verifica si existe el buildNumber en el TFS
+        string ObtenerRutaBuild(string rutaTfsBase, int numeroBuild); //Obtiene la ruta del build donde se encuentran los archivos
+        List<string> BuscarCompilados(string rutaBuild, string nombreModulo); //Enlista y busca los compilados existentes.
     }
 }
