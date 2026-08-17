aad68536ddf3b53b7c053a98e8fbaff98552e8f3
DAY-V <DAY-V@DESKTOP-PLM74CT>
2026-08-17 11:37:50 -0600

    Creacion de las interfaces del proyecto

--- GenerarPaquetes.DAO/Interfaces/IFileSystemDao.cs
+++ GenerarPaquetes.DAO/Interfaces/IFileSystemDao.cs
@@ -8,5 +8,14 @@ namespace GenerarPaquetes.DAO.Interfaces
 {
     internal interface IFileSystemDao
     {
+        void CrearDirectorio(string ruta);
+        bool ExisteArchivo(string ruta);
+        bool ExisteDirectorio(string ruta);
+        void CopiarArchivo(string origen, string destino, bool sobreescribir = true);
+        void CopiarDirectorioRecursivo(string origen, string destino);
+        string[] LeerLineasDeArchivo(string ruta);
+        void EscribirLineasDeArchivo(string ruta, string[] lineas);
+        string[] ObtenerArchivosDelDirectorio(string ruta, string patronBusqueda);
+        string[] ObtenerLosSubdirectorios(string ruta, string patronBusqueda, SearchOption op = SearchOption.TopDirectoryOnly);
     }
 }
--- GenerarPaquetes.DAO/Interfaces/ITfsMockDao.cs
+++ GenerarPaquetes.DAO/Interfaces/ITfsMockDao.cs
@@ -8,5 +8,8 @@ namespace GenerarPaquetes.DAO.Interfaces
 {
     internal interface ITfsMockDao
     {
+        bool ExisteBuild(string rutaTfsBase, int numeroBuild);
+        string ObtenerRutaBuild(string rutaTfsBase, int numeroBuild);
+        List<string> BuscarCompilados(string rutaBuild, string nombreModulo);
     }
 }
