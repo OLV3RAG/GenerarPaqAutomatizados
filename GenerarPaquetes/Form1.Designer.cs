63a8d960fd9271616995edaa0932ee4c32f33663
DAY-V <DAY-V@DESKTOP-PLM74CT>
2026-08-19 11:47:15 -0600

    Cambio de nombres de los selectedButtons

--- GenerarPaquetes/Form1.Designer.cs
+++ GenerarPaquetes/Form1.Designer.cs
@@ -53,12 +53,9 @@
             this.checkBox17 = new System.Windows.Forms.CheckBox();
             this.checkBox16 = new System.Windows.Forms.CheckBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.checkBox26 = new System.Windows.Forms.CheckBox();
             this.checkBox21 = new System.Windows.Forms.CheckBox();
             this.checkBox22 = new System.Windows.Forms.CheckBox();
             this.checkBox23 = new System.Windows.Forms.CheckBox();
-            this.checkBox24 = new System.Windows.Forms.CheckBox();
-            this.checkBox25 = new System.Windows.Forms.CheckBox();
             this.checkBox18 = new System.Windows.Forms.CheckBox();
             this.checkBox19 = new System.Windows.Forms.CheckBox();
             this.checkBox20 = new System.Windows.Forms.CheckBox();
@@ -141,9 +138,9 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(53, 31);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.Size = new System.Drawing.Size(118, 20);
             this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "BatchLauncher";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
@@ -152,9 +149,9 @@
             this.checkBox2.AutoSize = true;
             this.checkBox2.Location = new System.Drawing.Point(53, 94);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(95, 20);
+            this.checkBox2.Size = new System.Drawing.Size(160, 20);
             this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.Text = "BusinessServiceSIAP";
             this.checkBox2.UseVisualStyleBackColor = true;
             this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
@@ -163,9 +160,9 @@
             this.checkBox3.AutoSize = true;
             this.checkBox3.Location = new System.Drawing.Point(53, 159);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(95, 20);
+            this.checkBox3.Size = new System.Drawing.Size(105, 20);
             this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.Text = "CatalogsWS";
             this.checkBox3.UseVisualStyleBackColor = true;
             this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
             // 
@@ -174,9 +171,9 @@
             this.checkBox4.AutoSize = true;
             this.checkBox4.Location = new System.Drawing.Point(53, 233);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(95, 20);
+            this.checkBox4.Size = new System.Drawing.Size(110, 20);
             this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.Text = "CloseService";
             this.checkBox4.UseVisualStyleBackColor = true;
             this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
             // 
@@ -185,9 +182,9 @@
             this.checkBox5.AutoSize = true;
             this.checkBox5.Location = new System.Drawing.Point(53, 309);
             this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(95, 20);
+            this.checkBox5.Size = new System.Drawing.Size(57, 20);
             this.checkBox5.TabIndex = 4;
-            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.Text = "DTS";
             this.checkBox5.UseVisualStyleBackColor = true;
             // 
             // checkBox10
@@ -195,9 +192,9 @@
             this.checkBox10.AutoSize = true;
             this.checkBox10.Location = new System.Drawing.Point(338, 31);
             this.checkBox10.Name = "checkBox10";
-            this.checkBox10.Size = new System.Drawing.Size(102, 20);
+            this.checkBox10.Size = new System.Drawing.Size(88, 20);
             this.checkBox10.TabIndex = 5;
-            this.checkBox10.Text = "checkBox10";
+            this.checkBox10.Text = "IDC_SIAP";
             this.checkBox10.UseVisualStyleBackColor = true;
             this.checkBox10.CheckedChanged += new System.EventHandler(this.checkBox10_CheckedChanged);
             // 
@@ -206,9 +203,9 @@
             this.checkBox9.AutoSize = true;
             this.checkBox9.Location = new System.Drawing.Point(338, 94);
             this.checkBox9.Name = "checkBox9";
-            this.checkBox9.Size = new System.Drawing.Size(95, 20);
+            this.checkBox9.Size = new System.Drawing.Size(108, 20);
             this.checkBox9.TabIndex = 6;
-            this.checkBox9.Text = "checkBox9";
+            this.checkBox9.Text = "IDC_SIAPApi";
             this.checkBox9.UseVisualStyleBackColor = true;
             this.checkBox9.CheckedChanged += new System.EventHandler(this.checkBox9_CheckedChanged);
             // 
@@ -217,9 +214,9 @@
             this.checkBox8.AutoSize = true;
             this.checkBox8.Location = new System.Drawing.Point(338, 159);
             this.checkBox8.Name = "checkBox8";
-            this.checkBox8.Size = new System.Drawing.Size(95, 20);
+            this.checkBox8.Size = new System.Drawing.Size(80, 20);
             this.checkBox8.TabIndex = 7;
-            this.checkBox8.Text = "checkBox8";
+            this.checkBox8.Text = "IDCMAS";
             this.checkBox8.UseVisualStyleBackColor = true;
             this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
             // 
@@ -228,9 +225,9 @@
             this.checkBox7.AutoSize = true;
             this.checkBox7.Location = new System.Drawing.Point(338, 233);
             this.checkBox7.Name = "checkBox7";
-            this.checkBox7.Size = new System.Drawing.Size(95, 20);
+            this.checkBox7.Size = new System.Drawing.Size(156, 20);
             this.checkBox7.TabIndex = 8;
-            this.checkBox7.Text = "checkBox7";
+            this.checkBox7.Text = "InstalarPortalAPIRest";
             this.checkBox7.UseVisualStyleBackColor = true;
             this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
             // 
@@ -239,9 +236,9 @@
             this.checkBox6.AutoSize = true;
             this.checkBox6.Location = new System.Drawing.Point(338, 309);
             this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(95, 20);
+            this.checkBox6.Size = new System.Drawing.Size(91, 20);
             this.checkBox6.TabIndex = 9;
-            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.Text = "InterfazAdi";
             this.checkBox6.UseVisualStyleBackColor = true;
             // 
             // checkBox15
@@ -249,9 +246,9 @@
             this.checkBox15.AutoSize = true;
             this.checkBox15.Location = new System.Drawing.Point(648, 31);
             this.checkBox15.Name = "checkBox15";
-            this.checkBox15.Size = new System.Drawing.Size(114, 20);
+            this.checkBox15.Size = new System.Drawing.Size(87, 20);
             this.checkBox15.TabIndex = 10;
-            this.checkBox15.Text = "ServiciosMAS";
+            this.checkBox15.Text = "MASWeb";
             this.checkBox15.UseVisualStyleBackColor = true;
             this.checkBox15.CheckedChanged += new System.EventHandler(this.checkBox15_CheckedChanged);
             // 
@@ -260,9 +257,9 @@
             this.checkBox14.AutoSize = true;
             this.checkBox14.Location = new System.Drawing.Point(648, 94);
             this.checkBox14.Name = "checkBox14";
-            this.checkBox14.Size = new System.Drawing.Size(102, 20);
+            this.checkBox14.Size = new System.Drawing.Size(94, 20);
             this.checkBox14.TabIndex = 11;
-            this.checkBox14.Text = "checkBox14";
+            this.checkBox14.Text = "PortalCFDI";
             this.checkBox14.UseVisualStyleBackColor = true;
             this.checkBox14.CheckedChanged += new System.EventHandler(this.checkBox14_CheckedChanged);
             // 
@@ -271,9 +268,9 @@
             this.checkBox13.AutoSize = true;
             this.checkBox13.Location = new System.Drawing.Point(648, 159);
             this.checkBox13.Name = "checkBox13";
-            this.checkBox13.Size = new System.Drawing.Size(102, 20);
+            this.checkBox13.Size = new System.Drawing.Size(151, 20);
             this.checkBox13.TabIndex = 12;
-            this.checkBox13.Text = "checkBox13";
+            this.checkBox13.Text = "PortalDatosFiscales";
             this.checkBox13.UseVisualStyleBackColor = true;
             this.checkBox13.CheckedChanged += new System.EventHandler(this.checkBox13_CheckedChanged);
             // 
@@ -282,9 +279,9 @@
             this.checkBox12.AutoSize = true;
             this.checkBox12.Location = new System.Drawing.Point(648, 233);
             this.checkBox12.Name = "checkBox12";
-            this.checkBox12.Size = new System.Drawing.Size(102, 20);
+            this.checkBox12.Size = new System.Drawing.Size(153, 20);
             this.checkBox12.TabIndex = 13;
-            this.checkBox12.Text = "checkBox12";
+            this.checkBox12.Text = "ProcesarMovimiento";
             this.checkBox12.UseVisualStyleBackColor = true;
             this.checkBox12.CheckedChanged += new System.EventHandler(this.checkBox12_CheckedChanged);
             // 
@@ -293,9 +290,9 @@
             this.checkBox11.AutoSize = true;
             this.checkBox11.Location = new System.Drawing.Point(648, 309);
             this.checkBox11.Name = "checkBox11";
-            this.checkBox11.Size = new System.Drawing.Size(102, 20);
+            this.checkBox11.Size = new System.Drawing.Size(115, 20);
             this.checkBox11.TabIndex = 14;
-            this.checkBox11.Text = "checkBox11";
+            this.checkBox11.Text = "QuotationWeb";
             this.checkBox11.UseVisualStyleBackColor = true;
             this.checkBox11.CheckedChanged += new System.EventHandler(this.checkBox11_CheckedChanged);
             // 
@@ -304,9 +301,9 @@
             this.checkBox17.AutoSize = true;
             this.checkBox17.Location = new System.Drawing.Point(904, 233);
             this.checkBox17.Name = "checkBox17";
-            this.checkBox17.Size = new System.Drawing.Size(102, 20);
+            this.checkBox17.Size = new System.Drawing.Size(126, 20);
             this.checkBox17.TabIndex = 18;
-            this.checkBox17.Text = "checkBox17";
+            this.checkBox17.Text = "ServicioEmision";
             this.checkBox17.UseVisualStyleBackColor = true;
             this.checkBox17.CheckedChanged += new System.EventHandler(this.checkBox17_CheckedChanged);
             // 
@@ -315,20 +312,17 @@
             this.checkBox16.AutoSize = true;
             this.checkBox16.Location = new System.Drawing.Point(904, 309);
             this.checkBox16.Name = "checkBox16";
-            this.checkBox16.Size = new System.Drawing.Size(102, 20);
+            this.checkBox16.Size = new System.Drawing.Size(144, 20);
             this.checkBox16.TabIndex = 19;
-            this.checkBox16.Text = "checkBox16";
+            this.checkBox16.Text = "ServicioIntegracion";
             this.checkBox16.UseVisualStyleBackColor = true;
             this.checkBox16.CheckedChanged += new System.EventHandler(this.checkBox16_CheckedChanged);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.checkBox26);
             this.panel1.Controls.Add(this.checkBox21);
             this.panel1.Controls.Add(this.checkBox22);
             this.panel1.Controls.Add(this.checkBox23);
-            this.panel1.Controls.Add(this.checkBox24);
-            this.panel1.Controls.Add(this.checkBox25);
             this.panel1.Controls.Add(this.checkBox16);
             this.panel1.Controls.Add(this.checkBox17);
             this.panel1.Controls.Add(this.checkBox18);
@@ -355,80 +349,48 @@
             this.panel1.TabIndex = 7;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // checkBox26
-            // 
-            this.checkBox26.AutoSize = true;
-            this.checkBox26.Location = new System.Drawing.Point(648, 370);
-            this.checkBox26.Name = "checkBox26";
-            this.checkBox26.Size = new System.Drawing.Size(102, 20);
-            this.checkBox26.TabIndex = 25;
-            this.checkBox26.Text = "checkBox26";
-            this.checkBox26.UseVisualStyleBackColor = true;
-            this.checkBox26.CheckedChanged += new System.EventHandler(this.checkBox26_CheckedChanged);
-            // 
             // checkBox21
             // 
             this.checkBox21.AutoSize = true;
-            this.checkBox21.Location = new System.Drawing.Point(1157, 309);
+            this.checkBox21.Location = new System.Drawing.Point(1108, 31);
             this.checkBox21.Name = "checkBox21";
-            this.checkBox21.Size = new System.Drawing.Size(102, 20);
+            this.checkBox21.Size = new System.Drawing.Size(201, 20);
             this.checkBox21.TabIndex = 24;
-            this.checkBox21.Text = "checkBox21";
+            this.checkBox21.Text = "ServicioIntegracionPermisos";
             this.checkBox21.UseVisualStyleBackColor = true;
             this.checkBox21.CheckedChanged += new System.EventHandler(this.checkBox21_CheckedChanged);
             // 
             // checkBox22
             // 
             this.checkBox22.AutoSize = true;
-            this.checkBox22.Location = new System.Drawing.Point(1157, 233);
+            this.checkBox22.Location = new System.Drawing.Point(1174, 159);
             this.checkBox22.Name = "checkBox22";
-            this.checkBox22.Size = new System.Drawing.Size(102, 20);
+            this.checkBox22.Size = new System.Drawing.Size(114, 20);
             this.checkBox22.TabIndex = 23;
-            this.checkBox22.Text = "checkBox22";
+            this.checkBox22.Text = "ServiciosMAS";
+            this.checkBox22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.checkBox22.UseVisualStyleBackColor = true;
             this.checkBox22.CheckedChanged += new System.EventHandler(this.checkBox22_CheckedChanged);
             // 
             // checkBox23
             // 
             this.checkBox23.AutoSize = true;
-            this.checkBox23.Location = new System.Drawing.Point(1157, 159);
+            this.checkBox23.Location = new System.Drawing.Point(1132, 309);
             this.checkBox23.Name = "checkBox23";
-            this.checkBox23.Size = new System.Drawing.Size(102, 20);
+            this.checkBox23.Size = new System.Drawing.Size(156, 20);
             this.checkBox23.TabIndex = 22;
-            this.checkBox23.Text = "checkBox23";
+            this.checkBox23.Text = "WS-CLPortalAgentes";
             this.checkBox23.UseVisualStyleBackColor = true;
             this.checkBox23.CheckedChanged += new System.EventHandler(this.checkBox23_CheckedChanged);
-            // 
-            // checkBox24
-            // 
-            this.checkBox24.AutoSize = true;
-            this.checkBox24.Location = new System.Drawing.Point(1157, 94);
-            this.checkBox24.Name = "checkBox24";
-            this.checkBox24.Size = new System.Drawing.Size(102, 20);
-            this.checkBox24.TabIndex = 21;
-            this.checkBox24.Text = "checkBox24";
-            this.checkBox24.UseVisualStyleBackColor = true;
-            this.checkBox24.CheckedChanged += new System.EventHandler(this.checkBox24_CheckedChanged);
-            // 
-            // checkBox25
-            // 
-            this.checkBox25.AutoSize = true;
-            this.checkBox25.Location = new System.Drawing.Point(1157, 31);
-            this.checkBox25.Name = "checkBox25";
-            this.checkBox25.Size = new System.Drawing.Size(102, 20);
-            this.checkBox25.TabIndex = 20;
-            this.checkBox25.Text = "checkBox25";
-            this.checkBox25.UseVisualStyleBackColor = true;
-            this.checkBox25.CheckedChanged += new System.EventHandler(this.checkBox25_CheckedChanged);
             // 
             // checkBox18
             // 
             this.checkBox18.AutoSize = true;
             this.checkBox18.Location = new System.Drawing.Point(904, 159);
             this.checkBox18.Name = "checkBox18";
-            this.checkBox18.Size = new System.Drawing.Size(102, 20);
+            this.checkBox18.Size = new System.Drawing.Size(172, 20);
             this.checkBox18.TabIndex = 17;
-            this.checkBox18.Text = "checkBox18";
+            this.checkBox18.Text = "ServicioDocumentacion";
             this.checkBox18.UseVisualStyleBackColor = true;
             this.checkBox18.CheckedChanged += new System.EventHandler(this.checkBox18_CheckedChanged);
             // 
@@ -437,9 +399,9 @@
             this.checkBox19.AutoSize = true;
             this.checkBox19.Location = new System.Drawing.Point(904, 94);
             this.checkBox19.Name = "checkBox19";
-            this.checkBox19.Size = new System.Drawing.Size(102, 20);
+            this.checkBox19.Size = new System.Drawing.Size(190, 20);
             this.checkBox19.TabIndex = 16;
-            this.checkBox19.Text = "checkBox19";
+            this.checkBox19.Text = "ServiceAutomaticLoadSIRI";
             this.checkBox19.UseVisualStyleBackColor = true;
             this.checkBox19.CheckedChanged += new System.EventHandler(this.checkBox19_CheckedChanged);
             // 
@@ -448,9 +410,9 @@
             this.checkBox20.AutoSize = true;
             this.checkBox20.Location = new System.Drawing.Point(904, 31);
             this.checkBox20.Name = "checkBox20";
-            this.checkBox20.Size = new System.Drawing.Size(102, 20);
+            this.checkBox20.Size = new System.Drawing.Size(100, 20);
             this.checkBox20.TabIndex = 15;
-            this.checkBox20.Text = "checkBox20";
+            this.checkBox20.Text = "Robot_CFD";
             this.checkBox20.UseVisualStyleBackColor = true;
             this.checkBox20.CheckedChanged += new System.EventHandler(this.checkBox20_CheckedChanged);
             // 
@@ -534,12 +496,9 @@
         private System.Windows.Forms.CheckBox checkBox17;
         private System.Windows.Forms.CheckBox checkBox16;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.CheckBox checkBox26;
         private System.Windows.Forms.CheckBox checkBox21;
         private System.Windows.Forms.CheckBox checkBox22;
         private System.Windows.Forms.CheckBox checkBox23;
-        private System.Windows.Forms.CheckBox checkBox24;
-        private System.Windows.Forms.CheckBox checkBox25;
         private System.Windows.Forms.CheckBox checkBox18;
         private System.Windows.Forms.CheckBox checkBox19;
         private System.Windows.Forms.CheckBox checkBox20;
